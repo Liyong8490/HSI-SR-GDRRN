426e31f8d1a16d06957da9077c7f64892f76b32a
Liyong8490 <liyong_xgd@outlook.com>
2018-11-06 16:39:34 +0800

    fix the bug when compute SAM and PSNR in validation. Now the results is as same as Matlab version.

--- utils.py
+++ utils.py
@@ -52,16 +52,21 @@ def batch_PSNR(im_true, im_fake, data_range):
     return torch.mean(psnr)
 
 def PSNR_GPU(im_true, im_fake):
-    data_range = 1
+    im_true *= 255
+    im_fake *= 255
+    im_true = im_true.round()
+    im_fake = im_fake.round()
+    data_range = 255
+    esp = 1e-12
     C = im_true.size()[0]
     H = im_true.size()[1]
     W = im_true.size()[2]
-    Itrue = im_true.clone().resize_(C*H*W)
-    Ifake = im_fake.clone().resize_(C*H*W)
+    Itrue = im_true.clone()
+    Ifake = im_fake.clone()
     mse = nn.MSELoss(reduce=False)
-    err = mse(Itrue, Ifake).sum(dim=0, keepdim=True).div_(C*H*W)
-    psnr = 10. * torch.log((data_range**2)/err) / np.log(10.)
-    return torch.mean(psnr)
+    err = mse(Itrue, Ifake).sum() / (C*H*W)
+    psnr = 10. * np.log((data_range**2)/(err.data[0] + esp)) / np.log(10.)
+    return psnr
 
 def batch_SAM_GPU(im_true, im_fake):
     N = im_true.size()[0]
@@ -82,15 +87,17 @@ def SAM_GPU(im_true, im_fake):
     C = im_true.size()[0]
     H = im_true.size()[1]
     W = im_true.size()[2]
-    Itrue = im_true.clone().resize_(C, H*W)
-    Ifake = im_fake.clone().resize_(C, H*W)
-    nom = torch.mul(Itrue, Ifake).sum(dim=0).resize_(H*W)
-    denom1 = torch.pow(Itrue,2).sum(dim=0).sqrt_().resize_(H*W)
-    denom2 = torch.pow(Ifake,2).sum(dim=0).sqrt_().resize_(H*W)
-    sam = torch.div(nom, torch.mul(denom1, denom2)).acos_().resize_(H*W)
-    sam = sam / np.pi * 180
-    sam = torch.sum(sam) / (H*W)
-    return sam
+    esp = 1e-12
+    Itrue = im_true.clone()#.resize_(C, H*W)
+    Ifake = im_fake.clone()#.resize_(C, H*W)
+    nom = torch.mul(Itrue, Ifake).sum(dim=0)#.resize_(H*W)
+    denominator = Itrue.norm(p=2, dim=0, keepdim=True).clamp(min=esp) * \
+                  Ifake.norm(p=2, dim=0, keepdim=True).clamp(min=esp)
+    denominator = denominator.squeeze()
+    sam = torch.div(nom, denominator).acos()
+    sam[sam != sam] = 0
+    sam_sum = torch.sum(sam) / (H * W) / np.pi * 180
+    return sam_sum
 
 def batch_SAM_CPU(im_true, im_fake):
     I_true = im_true.data.cpu().numpy()
